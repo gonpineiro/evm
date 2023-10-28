aca36deec473f9498b1865ab3ba0a29c16aab061
Gonzalo <gonzalo@estadodigital.com>
2023-10-28 09:57:59 -0300

    GasDinamico

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,25 +5,25 @@ import ExecutionContext from "../classes/execution";
 import LevelDB from "../classes/storage";
 import { DB_PATH } from "../constants";
 
-interface PVMOptions {
+interface EVCOptions {
   code: string;
   gasLimit: number;
 }
 
-const pvm: Command<PVMOptions> = {
-  name: "pvm",
+const evm: Command<EVCOptions> = {
+  name: "evm",
   description: "Ethereum Virtual Machine execution environment",
   optionDefinitions: [
     {
       name: "code",
-      description: "Bytecode to execute by the PVM",
+      description: "Bytecode to execute by the EVM",
       type: "string",
       alias: "c",
       isRequired: true,
     },
     {
       name: "gasLimit",
-      description: "Available gas for the PVM execution",
+      description: "Available gas for the EVM execution",
       type: "number",
       alias: "g",
       isRequired: true,
@@ -43,4 +43,4 @@ const pvm: Command<PVMOptions> = {
   },
 };
 
-runCli({ rootCommand: pvm, argv: process.argv });
+runCli({ rootCommand: evm, argv: process.argv });
